192217b7c045708bfc3d22be5409964dddf44b4a
Jakub Kania <kubak.15@gmail.com>
2018-07-11 11:46:41 +0200

    implement layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withStyles, CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 import * as api from './api'
+import * as urls from './urls';
+import LoginPage from './containers/loginpage';
+import Layout from './containers/layout';
 
 window.zepp = api;
+
+const styles = {
+
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Router>
+          <CssBaseline>
+            <Switch>
+              <Route exact path={urls.ROOT} component={Layout} />
+              <Route path={urls.LOGIN} component={LoginPage} />
+            </Switch>
+          </CssBaseline>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/urls.js
@@ -0,0 +1,13 @@
+//zmienne jakie będą w adresie przegladrki
+
+
+export const ROOT = '/';
+export const LOGIN = '/login';
+export const PROJECT_LIST = '/projects';
+export const PROJECT_EDIT = '/projects/:id';
+
+function editProject(projId) {
+    return PROJECT_EDIT.replace(":id", projId);
+}
+
+export const PROJECT_CREATE = '/projects/new';
